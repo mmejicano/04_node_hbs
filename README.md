54d1b1057bebb1878ea1591bd46f67a1e6f6e15e
Marvin Mejicano <mejicanomarvin@gmail.com>
2022-02-03 15:12:43 -0600

    path resolve alternative to __Dirname

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 # Aplicacion servidor
 
-
 # Conocimientos aplicados
 
 1. librerias: express
-http://expressjs.com/en/starter/hello-world.html
+   http://expressjs.com/en/starter/hello-world.html
 2. template engine: hbs
-3. __dirname no está definida en proyectos con ES Modules solo en CommonJS
+3. \_\_dirname no está definida en proyectos con ES Modules solo en CommonJS: const \_\_dirname = path.resolve('./');
 
-4. contenido estatico (public) + envio datos a views + {{data}}
-5. partials {> header title=}
-6. nodemon con hbs
+4) contenido estatico (public: tiene prioridad sobre otras rutas) + envio datos a views + {{data}}
+5) partials {> header title=}
+6) nodemon con hbs
 
+7) express: routes, middleware
+8) request(peticion) ,response(respuesta)
+9) JSON.stringlify()
+10) Headers: content-type: application/json|CSV
 
-7. express: routes, middleware
-
+11) routes: app.get("\*", fn)
+12) middleware: static
--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-import express from "express";
+import express from 'express';
+import path from 'path';
 
 const app = express();
 const port = 8000;
-const { pathname: root } = new URL("../src", import.meta.url);
+// const { pathname: root } = new URL('../src', import.meta.url);
+const __dirname = path.resolve('./');
 
-app.set("view engine", "hbs");
-app.set("views", root + "/views");
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/src/views');
 
 // contenido estatico
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 //app.get('/', function (req,res) {
 //  res.send('Pagina personal')
 //})
 
-app.get("/home", function (req, res) {
-  res.render("home", {titulo: 'Home Page'}); // no nesita extension
+app.get('/home', function (req, res) {
+  res.render('home', { titulo: 'Home Page' }); // no nesita extension
 });
-app.get("*", function (req, res) {
-  res.sendFile(root + "/../public/error.html");
+
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/public/error.html');
 });
 
 app.listen(port, () => {
